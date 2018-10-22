28b451a38ea88651e53c268f88550585925d0685
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-22 10:10:57 -0300

    NoCutoutOverlay: override others notch overlays
    Change-Id: If04a579b35b004517167ec49256ee5f1d129e6de

--- overlay/common/packages/apps/overlays/NoCutoutOverlay/NoCutoutOverlay/Android.mk
+++ overlay/common/packages/apps/overlays/NoCutoutOverlay/NoCutoutOverlay/Android.mk
@@ -7,5 +7,6 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := NoCutoutOverlay
 LOCAL_SDK_VERSION := current
 LOCAL_PRIVILEGED_MODULE := false
+LOCAL_OVERRIDES_PACKAGES := DisplayCutoutEmulationCornerOverlay DisplayCutoutEmulationDoubleOverlay DisplayCutoutEmulationNarrowOverlay DisplayCutoutEmulationTallOverlay DisplayCutoutEmulationWideOverlay
 
 include $(BUILD_PACKAGE)
