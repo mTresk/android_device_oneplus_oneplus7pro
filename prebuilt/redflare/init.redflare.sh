9fb467dbf863e81454ff6f4593bcf427b53ec875
mTresk <djtresk@gmail.com>
2019-01-05 16:03:51 +0100

    Adjust kernel settings

--- prebuilt/redflare/init.redflare.sh
+++ prebuilt/redflare/init.redflare.sh
@@ -6,19 +6,21 @@ sleep 25;
 
 # Setup Schedutil Governor
     echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-    echo 200 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
+    echo 500 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
     echo 20000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
     echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
     echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
 
     echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor    
-    echo 200 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us
+    echo 500 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us
     echo 20000 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/down_rate_limit_us
     echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/pl
     echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/hispeed_freq
 
-# Stune configuration
-    echo 30 > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
+# Input boost and stune configuration
+    echo "0:1516800 1:0 2:0 3:0 4:0 5:0 6:0 7:0" > /sys/module/cpu_boost/parameters/input_boost_freq
+    echo 600 > /sys/module/cpu_boost/parameters/input_boost_ms
+    echo 15 > /sys/module/cpu_boost/parameters/dynamic_stune_boost
 
 # Disable Boost_No_Override
     echo 0 > /dev/stune/foreground/schedtune.sched_boost_no_override
@@ -31,6 +33,9 @@ sleep 25;
 
 # Enable PEWQ
     echo Y > /sys/module/workqueue/parameters/power_efficient
+
+# Disable Touchboost
+    echo 0 > /sys/module/msm_performance/parameters/touchboost
 
 # Adjust SCHED Features
     echo NO_EAS_USE_NEED_IDLE > /sys/kernel/debug/sched_features
@@ -46,5 +51,12 @@ sleep 25;
     echo 0-7 > /dev/cpuset/top-app/cpus
     # Since we are not using core rotator, lets load balance
     echo 0-3,6-7 > /dev/cpuset/foreground/cpus
-    echo 0-3 > /dev/cpuset/background/cpus
+    echo 0-1 > /dev/cpuset/background/cpus
     echo 0-3  > /dev/cpuset/system-background/cpus
+
+# For better screen off idle
+    echo 0-3 > /dev/cpuset/restricted/cpus
+
+# Adjust Read Ahead
+    echo 128 > /sys/block/sda/queue/read_ahead_kb
+    echo 128 > /sys/block/dm-0/queue/read_ahead_kb
