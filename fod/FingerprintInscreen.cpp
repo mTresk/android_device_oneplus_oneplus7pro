891dfaf8f3026ffa918df3b48d8d1b16fc7ffadc
LuK1337 <priv.luk@gmail.com>
2020-06-03 10:53:33 +0200

    sm8150-common: fod: Reset everything when onShowFODView() is called
    * Perhaps it'll	fix random bright FOD icon?
    
    Change-Id: I08eaf75bfd8a5a6aea1f89ff6810e9c271e1cbe3

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -133,6 +133,9 @@ Return<void> FingerprintInscreen::onShowFODView() {
     set(NATIVE_DISPLAY_WIDE, 1);
 
     this->mFodCircleVisible = true;
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
 }
